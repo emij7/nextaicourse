bcbea3db9946277d811f203af838a139a298dca0
Emiliano Juarez <75501977+emij7@users.noreply.github.com>
2024-02-15 19:33:24 -0300

    Feat: client counter added

--- app/page.js
+++ app/page.js
@@ -1,15 +1,12 @@
 import Link from "next/link";
-import React from "react";
 
-const HomePage = () => {
+export default function Home() {
   return (
     <div>
-      <h1 className="text-7xl">Home Page</h1>
-      <Link href="about" className="text-2xl">
-        About page
+      <h1 className="text-5xl mb-8 font-bold">Next.js Tutorial</h1>
+      <Link href="/client" className="btn btn-accent">
+        Get started
       </Link>
     </div>
   );
-};
-
-export default HomePage;
+}
