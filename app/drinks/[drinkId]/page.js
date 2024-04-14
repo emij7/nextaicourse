165db3ecb0a9146b839b1f0a6755fea41b268b81
Emiliano Juarez <75501977+emij7@users.noreply.github.com>
2024-04-14 20:15:08 -0300

    Get drinks data individually.

--- app/drinks/[drinkId]/page.js
+++ app/drinks/[drinkId]/page.js
@@ -1,8 +1,29 @@
 import React from "react";
+import Link from "next/link";
+//en esta página se mantiene el loading y error del layout
+const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 
-const SingleDrinkPage = ({ params }) => {
-  console.log(params);
-  return <div>Param id: {params.drinkId}</div>;
+const getSingleDrink = async (id) => {
+  try {
+    const res = await fetch(`${url}${id}`);
+    return res.json();
+  } catch (error) {
+    throw new Error("Failed to fetch drink data");
+  }
+};
+
+const SingleDrinkPage = async ({ params }) => {
+  const data = await getSingleDrink(params.drinkId);
+  const title = data?.drinks[0]?.strDrink;
+  const imgSrc = data?.drinks[0]?.strDrinkThumb;
+  return (
+    <div>
+      <Link href="/drinks" className="btn btn-primary mt-8 mb-12">
+        back to drinks
+      </Link>
+      <h1 className="text-4xl mb-8">{title}</h1>
+    </div>
+  );
 };
 
 export default SingleDrinkPage;
