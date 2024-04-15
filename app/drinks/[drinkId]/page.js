c4945d2846eb0fd7e6d53ca998ed88c09e63a3ed
Emiliano Juarez <75501977+emij7@users.noreply.github.com>
2024-04-15 20:22:12 -0300

    Drinks images

--- app/drinks/[drinkId]/page.js
+++ app/drinks/[drinkId]/page.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+
 //en esta página se mantiene el loading y error del layout
 const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 
@@ -16,11 +18,20 @@ const SingleDrinkPage = async ({ params }) => {
   const data = await getSingleDrink(params.drinkId);
   const title = data?.drinks[0]?.strDrink;
   const imgSrc = data?.drinks[0]?.strDrinkThumb;
+  console.log(imgSrc);
   return (
     <div>
       <Link href="/drinks" className="btn btn-primary mt-8 mb-12">
         back to drinks
       </Link>
+      <Image
+        src={imgSrc}
+        //Determina el aspect ratio del contenedor
+        width={300}
+        height={300}
+        className="w-48 h-48 rounded shadow-lg mb-4"
+        alt={title + " drink."}
+      />
       <h1 className="text-4xl mb-8">{title}</h1>
     </div>
   );
--- components/DrinkList.jsx
+++ components/DrinkList.jsx
@@ -1,19 +1,29 @@
 import React from "react";
-
-const DrinkList = ({ drinks }) => {
+import Image from "next/image";
+import Link from "next/link";
+const DrinksList = ({ drinks }) => {
   return (
-    <ul className="menu menu-vertical pl-0">
-      {drinks.map((drink) => {
-        return (
-          <li key={drink.idDrink} className="menu-item">
-            <a href={`/drinks/${drink.idDrink}`} className="menu-link">
-              {drink.strDrink}
-            </a>
-          </li>
-        );
-      })}
+    <ul className="grid sm:grid-cols-3 gap-6 mt-6">
+      {drinks.map((drink) => (
+        <li key={drink.idDrink}>
+          <Link
+            href={`/drinks/${drink.idDrink}`}
+            className="text-xl font-medium"
+          >
+            <div className="relative h-48 mb-4">
+              <Image
+                src={drink.strDrinkThumb}
+                fill
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw"
+                alt={drink.strDrink}
+                className="rounded-md object-cover"
+              />
+            </div>
+            {drink.strDrink}
+          </Link>
+        </li>
+      ))}
     </ul>
   );
 };
-
-export default DrinkList;
+export default DrinksList;
